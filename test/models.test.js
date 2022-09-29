8d786ee146425d9ee4e326ce765a9f728dd42493
Fabrício Galende Marques de Carvalho <fabriciogmc@gmail.com>
2022-09-29 18:10:43 -0300

    Correcting the error to be OK in CI

--- test/models.test.js
+++ test/models.test.js
@@ -1,6 +1,6 @@
 const model = require('../src/models/models');
 var Projeto = model.Projeto;
-var prj = new Projeto(2,'Novo CI','Software','JavaScript','01/01/01','02/01/01');
+var prj = new Projeto(1,'Novo CI','Software','JavaScript','01/01/01','02/01/01');
 
 test('Verifica o setter de uma propriedade', () => {
   expect(prj.titulo).toEqual('Novo CI');
